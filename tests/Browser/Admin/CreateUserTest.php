a8ecb701a1f6987b7663087860a6ce1a3d7977ef
Cesar Acual <chechaacual@gmail.com>
2019-11-09 00:15:39 -0600

    10.Creación de pruebas End-to-End con Laravel Dusk

---
+++ tests/Browser/Admin/CreateUserTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Browser\Admin;
+
+use App\{Profession, Skill, User};
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateUserTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function a_user_can_be_created()
+    {
+        $profession = factory(Profession::class)->create();
+        $skillA = factory(Skill::class)->create();
+        $skillB = factory(Skill::class)->create();
+
+        $this->browse(function (Browser $browser) use ($profession, $skillA, $skillB) {
+            $browser->visit('usuarios/nuevo')
+                ->type('name', 'Cesar')
+                ->type('email', 'checha@gmail.com')
+                ->type('password', 'laravel')
+                ->type('bio', 'Programador')
+                ->select('profession_id', $profession->id)
+                ->type('twitter', 'https://twitter.com/chechamas')
+                ->check("skills[{$skillA->id}]")
+                ->check("skills[{$skillB->id}]")
+                ->radio('role', 'user')
+                ->press('Crear usuario')
+                ->assertPathIs('/usuarios')
+                ->assertSee('Cesar')
+                ->assertSee('checha@gmail.com');
+        });
+
+        $this->assertCredentials([
+            'name' => 'Cesar',
+            'email' => 'checha@gmail.com',
+            'password' => 'laravel',
+            'role' => 'user',
+        ]);
+
+        $user = User::findByEmail('checha@gmail.com');
+
+        $this->assertDatabaseHas('user_profiles', [
+            'bio' => 'Programador',
+            'twitter' => 'https://twitter.com/chechamas',
+            'user_id' => $user->id,
+            'profession_id' => $profession->id,
+        ]);
+
+        $this->assertDatabaseHas('user_skill', [
+            'user_id' => $user->id,
+            'skill_id' => $skillA->id
+        ]);
+
+        $this->assertDatabaseHas('user_skill', [
+            'user_id' => $user->id,
+            'skill_id' => $skillB->id
+        ]);
+    }
+}
