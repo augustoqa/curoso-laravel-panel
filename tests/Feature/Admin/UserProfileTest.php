8b95dc68708e3a7a4aa60ae3d91f787b067d657c
Cesar Acual <chechaacual@gmail.com>
2019-11-11 19:13:44 -0600

    18.Asignación masiva en Eloquent ORM a fondo

---
+++ tests/Feature/Admin/UserProfileTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\{Profession, User, UserProfile};
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserProfileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $defaultData = [
+        'name' => 'Cesar',
+        'email' => 'checha@gmail.com',
+        'bio' => 'Programador Laravel y Vue.js',
+        'twitter' => 'https://twitter.com/chechamas'
+    ];
+
+    /** @test */
+    function a_user_can_edit_its_profile()
+    {
+        $user = factory(User::class)->create();
+        $user->profile()->save(factory(UserProfile::class)->make());
+
+        $newProfession = factory(Profession::class)->create();
+
+        //$this->actingAs($user);
+
+        $response = $this->get('/editar-perfil/');
+
+        $response->assertStatus(200);
+
+        $response = $this->put('/editar-perfil/', [
+            'name' => 'Cesar',
+            'email' => 'checha@gmail.com',
+            'bio' => 'Programador Laravel y Vue.js',
+            'twitter' => 'https://twitter.com/chechamas',
+            'profession_id' => $newProfession->id,
+        ]);
+
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Cesar',
+            'email' => 'checha@gmail.com',
+        ]);
+
+        $this->assertDatabaseHas('user_profiles', [
+            'bio' => 'Programador Laravel y Vue.js',
+            'twitter' => 'https://twitter.com/chechamas',
+            'profession_id' => $newProfession->id,
+        ]);
+    }
+
+    /** @test */
+    function the_user_cannot_change_its_role()
+    {
+        $user = factory(User::class)->create([
+            'role' => 'user'
+        ]);
+
+        $response = $this->put('/editar-perfil/', $this->withData([
+            'role' => 'admin',
+        ]));
+
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'role' => 'user',
+        ]);
+    }
+
+    /** @test */
+    function the_user_cannot_change_its_password()
+    {
+        factory(User::class)->create([
+            'password' => bcrypt('old123'),
+        ]);
+
+        $response = $this->put('/editar-perfil/', $this->withData([
+            'email' => 'checha@gmail.com',
+            'password' => 'new456'
+        ]));
+
+        $response->assertRedirect();
+
+        $this->assertCredentials([
+            'email' => 'checha@gmail.com',
+            'password' => 'old123',
+        ]);
+    }
+}
