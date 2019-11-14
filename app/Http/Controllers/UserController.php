38f533f5e123c7286dea86b545318d5c07739f73
Cesar Acual <augustoacual@gmail.com>
2019-11-14 17:46:20 -0600

    26.Busqueda avanzada con Eloquent usando whereHas y Scopes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,12 +18,7 @@ class UserController extends Controller
                     $query->doesntHave('team');
                 }
             })
-            ->when(request('search'), function ($query, $search) {
-                $query->where(function ($query) use ($search) {
-                    $query->where('name', 'like', "%{$search}%")
-                        ->orWhere('email', 'like', "%{$search}%");
-                });
-            })
+            ->search(request('search'))
             ->orderByDesc('created_at')
             ->paginate();
 
--- app/User.php
+++ app/User.php
@@ -61,4 +61,19 @@ class User extends Authenticatable
     {
         return $this->role === 'admin';
     }
+
+    public function scopeSearch($query, $search)
+    {
+        if (empty($search)) {
+            return;
+        }
+
+        $query->where(function ($query) use ($search) {
+            $query->where('name', 'like', "%{$search}%")
+                ->orWhere('email', 'like', "%{$search}%")
+                ->orWhereHas('team', function ($query) use ($search) {
+                    $query->where('name', 'like', "%{$search}%");
+                });
+        });
+    }
 }
