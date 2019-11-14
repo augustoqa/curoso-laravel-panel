80f8cbf5a5a490ce977cb42facec05014c88bbb2
Cesar Acual <augustoacual@gmail.com>
2019-11-13 23:59:10 -0600

    24.Creacion y asociacion de la tabla y modelo de equipos

--- app/User.php
+++ app/User.php
@@ -42,6 +42,11 @@ class User extends Authenticatable
         return static::where(compact('email'))->first();
     }
 
+    public function team()
+    {
+        return $this->belongsTo(Team::class)->withDefault();
+    }
+
     public function skills()
     {
         return $this->belongsToMany(Skill::class, 'user_skill');
