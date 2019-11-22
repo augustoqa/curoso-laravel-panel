7e9d972da2fb5090d2a1e8d28fa1831f7a078d81
Cesar Acual <chechaacual@gmail.com>
2019-11-22 12:15:34 -0600

    53.Orden de registros dinámico con subconsultas en Eloquent ORM

--- app/UserFilter.php
+++ app/UserFilter.php
@@ -7,12 +7,12 @@ namespace App;
 use App\Rules\SortableColumn;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class UserFilter extends QueryFilter
 {
     protected $aliases = [
         'date' => 'created_at',
+        'login' => 'last_login_at',
     ];
 
     public function rules(): array
@@ -24,7 +24,7 @@ class UserFilter extends QueryFilter
             'skills' => 'array|exists:skills,id',
             'from' => 'date_format:d/m/Y',
             'to' => 'date_format:d/m/Y',
-            'order' => [new SortableColumn(['name', 'email', 'date'])],
+            'order' => [new SortableColumn(['name', 'email', 'date', 'login'])],
         ];
     }
 
